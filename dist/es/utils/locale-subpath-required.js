bd779133987e99b9c84f3133263ddbf7b32de97c
Russell Dunphy <russell@russelldunphy.com>
2019-03-07 10:56:23 +0000

    push dist to github for easier inclusion of transpiled lib

---
+++ dist/es/utils/locale-subpath-required.js
@@ -0,0 +1,23 @@
+import { localeSubpathOptions } from '../config/default-config';
+export default ((nextI18NextConfig, lng) => {
+  const {
+    defaultLanguage,
+    localeSubpaths
+  } = nextI18NextConfig.config;
+
+  if (lng) {
+    if (localeSubpaths === localeSubpathOptions.NONE) {
+      return false;
+    }
+
+    if (localeSubpaths === localeSubpathOptions.FOREIGN && lng !== defaultLanguage) {
+      return true;
+    }
+
+    if (localeSubpaths === localeSubpathOptions.ALL) {
+      return true;
+    }
+  }
+
+  return false;
+});
